59051bc90285db55089a2ae055ff5b65abbf63c9
Christian Lascsak <christian.lascsak7@gmail.com>
2018-06-15 10:14:56 +0200

    figure added

--- deComp.m
+++ deComp.m
@@ -5,9 +5,10 @@
 function [low,high] = deComp (signal, decLowP, decHighP, steps)
   low = signal; 
   k=1;
+  figure;
   for i = 1 : steps
     [low,high] = dwt (low, decLowP, decHighP);
-    figure,subplot(6,2,k), plot(low),title("Approximation");
+    subplot(6,2,k), plot(low),title("Approximation");
     k = k+1;
     subplot(6,2,k),plot(high),title("Details");
     k=k+1;
--- edgeDetec1D.m
+++ edgeDetec1D.m
@@ -4,13 +4,16 @@ function [retval] = edgeDetec1D ()
    decLowP = [0.7071067812  0.7071067812];
    decHighP = [-0.7071067812 0.7071067812];
    
+   %Produce the signal
    signal = zeros(1,1024);
    signal(200:300) = 1;
    signal(600:700) = 1;
+   %add the noise
    noise = randn(1,1024) * 0.2;
    signal = signal.+noise;
+   
    gauss = generateGauss(5);
-   figure, plot(signal),title("Original");
+   figure, plot(signal),title("Original Signal with noise");
    [low,high] = deComp(signal, decLowP,decHighP,6);
    
 end
